fb543cb6d59ea76d8ee56a1494e64f6098347bc5
Vimalate <1902644369@qq.com>
2021-08-29 20:12:20 +0800

    ValidateInput

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
+    <a class="navbar-brand" href="#">知乎专栏</a>
+    <ul v-if="!user.isLogin" class="list-inline mb-0">
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2">登陆</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light my-2">注册</a>
+      </li>
+    </ul>
+    <ul v-else class="list-inline mb-0">
+      <li class="list-inline-item">
+        <Dropdown :title="`你好，${user.name}}`">
+          <dropdown-item
+            ><a href="#" class="dropdown-item">新建文章</a></dropdown-item
+          >
+          <dropdown-item
+            ><a href="#" class="dropdown-item">编辑资料</a></dropdown-item
+          >
+          <dropdown-item
+            ><a href="#" class="dropdown-item">退出登陆</a></dropdown-item
+          >
+        </Dropdown>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Dropdown from "@/components/Dropdown.vue";
+import DropdownItem from "./DropdownItem.vue";
+
+export interface useProps {
+  isLogin: boolean;
+  name?: string; // 可选
+  id?: number;
+}
+
+export default defineComponent({
+  name: "GlobalHeader",
+  components: {
+    Dropdown,
+    DropdownItem
+  },
+  props: {
+    user: {
+      type: Object as PropType<useProps>,
+      required: true
+    }
+  },
+  setup() {
+    return {};
+  }
+});
+</script>
+
+<style scoped>
+</style>
